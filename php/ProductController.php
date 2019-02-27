3bd69c1408883cdb12c3932a90cd94ef6e173b0f
Thembinkosi <terafumba@gmail.com>
2019-02-27 12:04:35 +0200

    Update ProductController.php

--- php/ProductController.php
+++ php/ProductController.php
@@ -1,34 +1,43 @@
 <?php
-
 namespace Realmdigital\Web\Controller;
-
 use DDesrosiers\SilexAnnotations\Annotations as SLX;
 use Silex\Application;
-
 /**
  * @SLX\Controller(prefix="product/")
  */
 class ProductController {
 
+    private $url = 'http://192.168.0.241/eanlist?type=Web';
+
     /**
-     * @SLX\Route(
-     *      @SLX\Request(method="GET", uri="/{id}")
-     * )
-     * @param Application $app
-     * @param $name
-     * @return
-     */
-    public function getById_GET(Application $app, $id){
+    * @param  $id can be null
+    * @param $name can be null
+    * @param $url 
+    */
+    public function initialiseCurl($id = null , $name = null)
+    {
         $curl = curl_init();
         $requestData = array();
-        $requestData['id'] = $id;
-        curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
+
+        if (isset($id)) 
+            $requestData['id'] = $id;
+
+        if (isset($name)) 
+            $requestData['names'] = $name;
+        
+        curl_setopt($curl, CURLOPT_URL,  $this->$url);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($curl);
         $response = json_decode($response);
         curl_close($curl);
+
+        return $response;
+    }
+
+    public function prepareCurlResponse($response)
+    {
         $result = [];
         for ($i =0; $i < count($response) ;$i++) {
             $prod = array();
@@ -45,10 +54,23 @@ class ProductController {
             }
             $result[] = $prod;
         }
-
-        return $app->render('products/product.detail.twig', $result);
+        return $result;
     }
 
+    /**
+     * @SLX\Route(
+     *      @SLX\Request(method="GET", uri="/{id}")
+     * )
+     * @param Application $app
+     * @param $name
+     * @return
+     */
+    public function getById(Application $app, $id){
+        $response = $this->initialiseCurl($id);
+        $result   = $this->prepareCurlResponse($response);
+      
+        return $app->render('products/product.detail.twig', $result);
+    }
     /**
      * @SLX\Route(
      *      @SLX\Request(method="GET", uri="/search/{name}")
@@ -57,35 +79,10 @@ class ProductController {
      * @param $name
      * @return
      */
-    public function getByName_GET(Application $app, $name){
-        $curl = curl_init();
-        $requestData = array();
-        $requestData['names'] = $name;
-        curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($curl);
-        $response = json_decode($response);
-        curl_close($curl);
-        $result = [];
-        for ($i =0; $i < count($response) ;$i++) {
-            $prod = array();
-            $prod['ean'] = $response[$i]['barcode'];
-            $prod["name"]= $response[$i]['itemName'];
-            $prod["prices"] = array();
-            for ($j=0;$j < count($response[$i]['prices']); $j++) {
-                if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
-                    $p_price = array();
-                    $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
-                    $p_price['currency'] = $response[$i]['prices'][$j]['currencyCode'];
-                    $prod["prices"][] = $p_price;
-                }
-            }
-            $result[] = $prod;
-        }
+    public function getByName(Application $app, $name){
+        $response = $this->initialiseCurl($id);
+        $result   = $this->prepareCurlResponse($response);
 
         return $app->render('products/products.twig', $result);
     }
-
 }
